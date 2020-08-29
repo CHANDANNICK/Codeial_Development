640544b2c230e22d52428b6fe13f93caacfc63de
CHANDAN KUMAR NICK <Chandannick@gmail.com>
2020-08-29 14:13:16 +0530

    1.Added Different Views

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,9 @@
 //module.exports.actionName = function(req,res){};
 
 module.exports.home = function (req, res) {
-  return res.send("<h1>Express is up and Running</h1>");
+  // return res.send("<h1>Express is up and Running</h1>");
+
+  return res.render("home", {
+    title: "Codeial",
+  });
 };
--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const port = 8000;
 const app = express();
+const expressLayouts = require("express-ejs-layouts");
+
+app.use(expressLayouts);
 
 //Middleware to Use Express Router
-
 app.use("/", require("./routes"));
+
+//Extracting Styles and Scripts of individual pages and place in the layout page
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 //Set up the views and view engine
 app.set("view engine", "ejs");
 app.set("views", "./views");
+app.use(express.urlencoded());
+app.use(express.static("assets"));
 
 app.listen(port, function (err) {
   if (err) {

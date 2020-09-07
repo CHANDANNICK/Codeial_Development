31fe4a235df3b653218c777191a0f385b1412df3
CHANDAN KUMAR NICK <Chandannick@gmail.com>
2020-09-07 14:02:29 +0530

    Showing posts by users on home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,20 @@
 //module.exports.actionName = function(req,res){};
+const Post = require("../models/post");
 
 module.exports.home = function (req, res) {
   // return res.send("<h1>Express is up and Running</h1>");
 
-  return res.render("home", {
-    title: "Codeial",
-    user: req.user,
-  });
+  //populate the user
+  Post.find({}).populate('user').exec(function (err, posts) {
+    if (err) {
+      console.log('Error in creating post');
+      return;
+    }
+    return res.render("home", {
+      title: "Codeial",
+      user: req.user,
+      posts : posts ,
+    });
+
+  })
 };

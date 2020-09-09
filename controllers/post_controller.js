f243bb487828c34ea9814ee112d2cc698bd10b31
CHANDAN KUMAR NICK <Chandannick@gmail.com>
2020-09-09 18:20:39 +0530

    Addded Delete Function

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,7 @@
 const Post = require("../models/post");
+const Comment = require("../models/comment");
+
+// Create Post
 
 module.exports.create = function (req, res) {
   Post.create(
@@ -14,4 +17,21 @@ module.exports.create = function (req, res) {
       return res.redirect("back");
     }
   );
+};
+
+// Delete Post and its comments
+
+module.exports.destroy = function (req, res) {
+  Post.findById(req.params.id, function (err, post) {
+    //.id means converting the object id into string
+    if (post.user == req.user.id) {
+      post.remove();
+
+      Comment.deleteMany({ post: req.params.id }, function (err) {
+        return res.redirect("back");
+      });
+    } else {
+      return res.redirect("back");
+    }
+  });
 };

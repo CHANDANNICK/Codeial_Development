80ba7267d6fe62588f95026c60001d833f7b1d38
CHANDAN KUMAR NICK <Chandannick@gmail.com>
2020-09-13 14:01:54 +0530

    Added update action of profile

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -10,11 +10,24 @@ module.exports.profile = function (req, res) {
     } else {
       return res.render("user_profile", {
         title: "Codeial",
-        user:req.user,
+        user: req.user,
         profile_user: usr,
       });
     }
   });
+};
+
+//Update Action
+module.exports.update = function (req, res) {
+  if (req.user.id == req.params.id) {
+    User.findByIdAndUpdate(req.params.id, req.body, function (err, user) {
+      if (user) {
+        return res.redirect("back");
+      }
+    });
+  } else {
+    return res.status(401).send("Unauthorized");
+  }
 };
 
 //render sign-up page
--- models/user.js
+++ models/user.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    username: {
+    name: {
       type: String,
       lowercase: true,
       unique: true,
